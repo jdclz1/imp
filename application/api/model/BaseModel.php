0d826c871a08c0870f930b9cbdd213b0fb4e2d9b
super <kdwmcyzw@163.com>
2019-10-25 23:38:53 +0800

    修改了图片的基础路径https

--- application/api/model/BaseModel.php
+++ application/api/model/BaseModel.php
@@ -8,13 +8,14 @@ class BaseModel extends Model
 {
 //    public function getUrlAttr($value,$data){ //读取器
     protected function prefixImgUrl ($value,$data){
-//        var_dump($data);   //这个值的数组
+//        var_dump($value);   //这个值的数组
 //        $c=$value;    //url这一列的值
         $finalUrl = $value;
         if($data['from'] == 1){
             $finalUrl = config('setting.img_prefix').$value;
         }
         return $finalUrl;
+//        var_dump($finalUrl);
 //        return config('setting.img_prefix').$value;
 
     }
